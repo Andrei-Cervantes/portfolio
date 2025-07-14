6babba57c3231809ef47dd153096b3c3927494f7
andrei <andrei29.cervantes@gmail.com>
2025-07-14 22:40:55 +0800

    setup: add grid pattern for landing bg

--- src/components/custom/MainContent/MainContent.tsx
+++ src/components/custom/MainContent/MainContent.tsx
@@ -1,6 +1,18 @@
+import { InteractiveGridPattern } from "@/components/ui/interactiveGridPattern";
+import { cn } from "@/lib/utils";
+
 const MainContent = () => {
     return (
-        <main className="flex-1">MainContent</main>
+        <main className="flex-1">
+            <section id="landing" className="min-h-100vh flex items-center justify-center">
+                <InteractiveGridPattern
+                    className={cn(
+                    "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
+                    "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+                    )}
+                />
+            </section>
+        </main>
     )
 }
 
