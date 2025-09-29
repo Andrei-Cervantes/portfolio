4a87118a9f548f327090dbf24ff27c1f24b7e897
Andrei Cervantes <140973981+Andrei-Cervantes@users.noreply.github.com>
2025-09-29 16:23:00 +0800

    add navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
 import ThemeToggle from "../components/ThemeToggle";
 import StarBackground from "../components/StarBackground";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-        <ThemeToggle />
-        <StarBackground />
-    </div>;
-}
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+      <StarBackground />
+      <Navbar />
+    </div>
+  );
+};
 
 export default Home;
