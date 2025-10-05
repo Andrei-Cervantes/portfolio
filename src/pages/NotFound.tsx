c0b0007853120856325d58ce25dcf53a4ba484ab
andrei <andrei29.cervantes@gmail.com>
2025-10-05 14:25:58 +0800

    improve styling for not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,18 @@
+import StarBackground from "../components/Common/StarBackground";
+import ThemeToggle from "../components/Common/ThemeToggle";
+
 const NotFound = () => {
-  return <div>404 - Page Not Found</div>;
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      <ThemeToggle />
+      <StarBackground />
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-3xl md:text-4xl font-bold">
+          <span className="text-primary">404</span> - Page Not Found
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
